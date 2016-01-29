291be7794238a18b54a11e71670ac00685a61fd0
SayDy <notforget@live.com>
2016-01-30 02:53:52 +0800

    Site updated: 2016-01-30 02:53:52

--- js/embed.js
+++ js/embed.js
@@ -16,7 +16,7 @@ function checkMobile() {
 //管理员判断开始
 function sskadmin(e) {
     var ssk = '';
-    if (e.user_id == 1778493287) {
+    if (e.user_id == 11040309) {
         if (checkMobile()) {
             ssk = '<span class="ua"><span class="sskadmin">博主</span></span><br><br>';
         } else {
